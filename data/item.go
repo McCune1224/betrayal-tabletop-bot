48f8ff36127999ac81a4e983c86ee5d004e1b8eb
McCune1224 <alexmccune1224@gmail.com>
2024-02-29 19:41:52 -0500

    Item DB representation

---
+++ data/item.go
@@ -0,0 +1,46 @@
+package data
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/lib/pq"
+)
+
+type Item struct {
+	ID          int            `db:"id"`
+	Name        string         `db:"name"`
+	Description string         `db:"description"`
+	Rarity      string         `db:"rarity"`
+	Cost        int            `db:"cost"`
+	Categories  pq.StringArray `db:"categories"`
+}
+
+type ItemModel struct {
+	Ddb *sqlx.DB
+}
+
+func (m *ItemModel) Get(id int) (*Item, error) {
+	var item Item
+	err := m.Ddb.Get(&item, "SELECT * FROM items WHERE id = $1", id)
+	if err != nil {
+		return nil, err
+	}
+	return &item, nil
+}
+
+func (m *ItemModel) GetAll() ([]*Item, error) {
+	var items []*Item
+	err := m.Ddb.Select(&items, "SELECT * FROM items")
+	if err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+func (m *ItemModel) GetByName(name string) (*Item, error) {
+	var item Item
+	err := m.Ddb.Get(&item, "SELECT * FROM items WHERE name ILIKE $1", name)
+	if err != nil {
+		return nil, err
+	}
+	return &item, nil
+}
